1d530e56c215a6c2ac183308ecde847d5a18c80e
david lee <david.hongwoo.lee@gmail.com>
2016-01-23 21:42:33 +0900

    add InMemoryAppender.

---
+++ include/plog/Appenders/InMemoryAppender.h
@@ -0,0 +1,36 @@
+/* #ifndef _IN_MEMORY_APPENDER_ */
+/* #define _IN_MEMORY_APPENDER_ */
+#pragma once
+
+#include <deque>
+
+namespace plog
+{
+	template<class Formatter>
+	class InMemoryAppender : public IAppender
+	{
+	public:
+		InMemoryAppender(size_t logMaxSize) : m_logMaxSize(logMaxSize)
+		{
+		}
+
+		virtual void write(const plog::Record& record)
+		{
+			std::string str = Formatter::format(record);
+			m_logList.push_back(str);
+			if (m_logList.size() > m_logMaxSize)
+				m_logList.pop_front();
+		}
+
+		const std::deque<std::string>& getLogList()
+		{
+			return m_logList;
+		}
+
+	private:
+		std::deque<std::string> m_logList;
+		const size_t m_logMaxSize;
+	};
+}
+
+//#endif // end of _IN_MEMORY_APPENDER_
---
+++ samples/InMemoryAppender/Main.cpp
@@ -0,0 +1,27 @@
+﻿//
+// CustomAppender - shows how to implement a custom appender that stores log messages in memory.
+//
+
+#include <plog/Log.h>
+#include <plog/Appenders/InMemoryAppender.h>
+#include <deque>
+#include <iostream>
+
+int main()
+{
+	static plog::InMemoryAppender<plog::TxtFormatter> myAppender(10); // Create our custom appender. 
+	plog::init(plog::debug, &myAppender); // Initialize the logger with our appender.
+
+	LOGD << "A debug message!";
+	LOGD << "A debug message 2!";
+
+	const std::deque<std::string>& logList = myAppender.getLogList(); // This returns a list of stored log messages. 
+
+	std::deque<std::string>::const_iterator iter = logList.begin();
+	std::deque<std::string>::const_iterator endIter = logList.end();
+	for (; iter != endIter; ++iter) {
+		std::cout << *iter;
+	}
+
+	return 0;
+}
